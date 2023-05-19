f9404dd31ccb802241fb7a025272356cbbfd8a6a
Tim Maupin <timmaupin@live.com>
2023-05-19 16:58:02 -0500

    begin making classes for Node and Linked list

--- javascript/code-challenges/401-code-challenges/Linked-list/index.js
+++ javascript/code-challenges/401-code-challenges/Linked-list/index.js
@@ -1,3 +1,17 @@
+'use strict';
 
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(){
+    this.head = null;
+  }
+  
+}
 
 
