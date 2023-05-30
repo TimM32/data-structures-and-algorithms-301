95776d7f8031ebec7df75f26f6752813cb686b5e
Tim Maupin <timmaupin@live.com>
2023-05-30 10:07:42 -0500

    create node and stack class

---
+++ javascript/code-challenges/401-code-challenges/Stacks-Que/index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack {
+  constructor(){
+    this.top = null;
+  }
+}
+
+push(value){
+  let newNode = new Node(value);
+  newNode.next = this.top;
+  this.top = newNode;
+}
+
+
+
