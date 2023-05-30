ec400af7e29302972d1737ba71976a33dc73d1d1
Tim Maupin <timmaupin@live.com>
2023-05-30 10:11:10 -0500

    add methods, peek is empty, pop to stack

--- javascript/code-challenges/401-code-challenges/Stacks-Que/index.js
+++ javascript/code-challenges/401-code-challenges/Stacks-Que/index.js
@@ -1,23 +1,42 @@
 'use strict';
 
 class Node {
-  constructor(value){
+  constructor(value) {
     this.value = value;
     this.next = null;
   }
 }
 
 class Stack {
-  constructor(){
+  constructor() {
     this.top = null;
+  }
+
+
+  push(value) {
+    let newNode = new Node(value);
+    newNode.next = this.top;
+    this.top = newNode;
+  }
+
+  pop() {
+    let removedValue = null;
+    if (this.top) {
+      removedValue = this.top.value;
+      this.top = this.top.next;
+    }
+    return removedValue;
+  }
+
+  peek() {
+    return this.top.value;
+  }
+
+  isEmpty() {
+    return this.top === null;
   }
 }
 
-push(value){
-  let newNode = new Node(value);
-  newNode.next = this.top;
-  this.top = newNode;
-}
 
 
 
