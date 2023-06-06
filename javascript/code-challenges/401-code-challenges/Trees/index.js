2bba04905cafc3965ab4eae86e41afc626968a96
TimM32 <timmaupin@live.com>
2023-06-06 16:03:05 -0500

    refactor maxValue tree

--- javascript/code-challenges/401-code-challenges/Trees/index.js
+++ javascript/code-challenges/401-code-challenges/Trees/index.js
@@ -69,18 +69,19 @@ class Tree {
 
   maxValue() {
     const results = [];
-
     const traverse = (node) => {
       if(node.left){
         traverse(node.left);
       }
-      results.push(node.value)
+      results.push(node.value);
+
       if(node.right){
         traverse(node.right);
       }
     };
     traverse(this.root);
-    return results.splice -1;
+    let maxValue = results.pop();
+    return maxValue;
   }
 }
 
