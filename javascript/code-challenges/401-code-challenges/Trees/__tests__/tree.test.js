221cd4194da986c8f7bcf84d9c89e15f99681975
TimM32 <timmaupin@live.com>
2023-06-07 10:26:21 -0500

    finish testing

--- javascript/code-challenges/401-code-challenges/Trees/__tests__/tree.test.js
+++ javascript/code-challenges/401-code-challenges/Trees/__tests__/tree.test.js
@@ -2,16 +2,71 @@
 
 const { Tree, Node, BinarySearch } = require(`../index.js`);
 
-// Code Challenge 15: Trees Tests
-describe(`Binary Tree`, () => {
-  let bst;
+
+describe(`BinarySearch and Tree`, () => {
+  let binarysearch;
   let tree;
   beforeEach(() => {
     binarysearch = new BinarySearch();
     tree = new Tree();
   });
 
-  // Can successfully instantiate an empty tree
+
   test(`Can successfully instantiate an empty tree`, () => {
     expect(bst.root).toBeNull();
   });
+
+  test(`Can successfully instantiate a tree with a single root`, () => {
+    bst.add(1);
+    expect(bst.root.value).toEqual(1);
+  });
+
+
+  test(`Can successfully add a left child and right child properly to a node for BinarySearch`, () => {
+    bst.add(1);
+    bst.add(2);
+    bst.add(3);
+    expect(bst.root.value).toEqual(1);
+    expect(bst.root.right.value).toEqual(2);
+    expect(bst.root.right.right.value).toEqual(3);
+  });
+
+
+  test(`Can successfully return a collection from a pre-order traversal`, () => {
+    bst.add(1);
+    bst.add(2);
+    bst.add(3);
+    expect(bst.preOrder()).toEqual([1, 2, 3]);
+  });
+
+
+   test(`Can successfully return a collection from an in-order traversal`, () => {
+    bst.add(1);
+
+    expect(bst.inOrder()).toEqual([1]);
+  });
+
+
+  test(`Can successfully return a collection from a post-order traversal`, () => {
+    bst.add(1);
+
+    expect(bst.postOrder()).toEqual([1]);
+  });
+
+
+  test(`Returns False for the contains method, given an existing or non-existing node value`, () => {
+    bst.add(1);
+    bst.add(2);
+    bst.add(3);
+    expect(bst.containsValue(4)).toBeFalsy();
+  });
+
+
+  test(`Returns True for the contains method, given an existing node value`, () => {
+    bst.add(1);
+    bst.add(2);
+    bst.add(3);
+    expect(bst.containsValue(3)).toBeTruthy();
+  });
+
+
