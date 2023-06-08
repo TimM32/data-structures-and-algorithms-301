7d6814b06868ad2eedb907fea28592ff01853ba4
Tim Maupin <timmaupin@live.com>
2023-06-08 11:35:28 -0500

    clean up the code for breadthfirst function

--- javascript/code-challenges/401-code-challenges/Trees/index.js
+++ javascript/code-challenges/401-code-challenges/Trees/index.js
@@ -66,6 +66,30 @@ class Tree {
     }
     return results;
   }
+
+  breadthFirst() {
+    if (!this.root) {
+      return [];
+    }
+
+    const queue = [this.root];
+    const results = [];
+
+    while (queue.length > 0) {
+      const node = queue.shift();
+      results.push(node.value);
+
+      if (node.left) {
+        queue.push(node.left);
+      }
+      if (node.right) {
+        queue.push(node.right);
+      }
+    }
+
+    return results;
+  }
+
 }
 
 
